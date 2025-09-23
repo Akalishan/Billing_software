206b996ad214560392852a4e5bf84b7b1b737064
Akalishan <168707927+Akalishan@users.noreply.github.com>
2025-09-23 22:57:50 +0530

    feat: Add fetch and delete operations for categories

--- Billing-software/src/main/java/com/example/Billing/software/repositary/CategoryRepository.java
+++ Billing-software/src/main/java/com/example/Billing/software/repositary/CategoryRepository.java
@@ -3,6 +3,9 @@ package com.example.Billing.software.repositary;
 import com.example.Billing.software.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long>{
 
+    static Optional<CategoryEntity> findByCategoryId(String categoryId);
 }
--- Billing-software/src/main/java/com/example/Billing/software/service/CategoryService.java
+++ Billing-software/src/main/java/com/example/Billing/software/service/CategoryService.java
@@ -3,6 +3,10 @@ package com.example.Billing.software.service;
 import com.example.Billing.software.io.CategoryRequest;
 import com.example.Billing.software.io.CategoryResponse;
 
+import java.util.List;
+
 public interface CategoryService {
     CategoryResponse add(CategoryRequest request);
+    List<CategoryResponse> read();
+    void  delete(String categoryId);
 }
