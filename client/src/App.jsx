60ed8e77070e53a733b438de56629b408548f1f8
Akalishan <168707927+Akalishan@users.noreply.github.com>
2025-12-16 08:26:29 +0530

    feat: Implement role-based access control for admin routes in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,25 +8,46 @@ import { ManageItems } from "./pages/ManageItems/ManageItems";
 import { Toaster } from "react-hot-toast";
 import Login from "./pages/Login/login";
 import { OrderHistory } from "./components/OrderHistory";
-
+import { useContext } from "react";
+import { AppContext } from "./context/AppContext.jsx";
+import { Navigate } from "react-router-dom";
 const App = () => {
   const location = useLocation();
+  const { auth } = useContext(AppContext);
+  const LoginRoute = ({ element }) => {
+    if (auth.token) {
+      return <Navigate to="/dashboard" />;
+    }
+    return element;
+  };
 
+  const protectedRoute = ({ element, allowedRoles }) => {
+    if (!auth.token) {
+      return <Navigate to="/login" />;
+    }
+    if (allowedRoles && !allowedRoles.includes(auth.role)) {
+      return <Navigate to="/dashboard" replace />;
+    }
+    return element;
+  };
   return (
     <div>
       {location.pathname !== "/login" && <Menubar />}
       <Toaster />
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/category" element={<ManageCategory />} />
-        <Route path="/users" element={<ManageUsers />} />
-        <Route path="/items" element={<ManageItems />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/login" element={<Login />} />
+        {/*These are the admin only routes */}
+        <Route path="/category" element={<protectedRoute element={<ManageCategory />} allowedRoles={["admin"]} />} />
+        <Route path="/users" element={<protectedRoute element={<ManageUsers />} allowedRoles={["admin"]} />} />
+        <Route path="/items" element={<protectedRoute element={<ManageItems />} allowedRoles={["admin"]} />} />
+
+        <Route path="/login" element={<LoginRoute element={<Login/>} />} />
         <Route path="/orders" element={<OrderHistory />} />
         <Route path="/" element={<Dashboard />} />
       </Routes>
     </div>
   );
 };
+
 export default App;
